6270b1a00781ebb31b7434e3c1b342a17d4b525c
Alessandra <alessaconceicao501501@gmail.com>
2026-03-22 13:54:06 -0400

    feat: added navbar in specific routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast'
+import { LoadingComponent } from './shared/components/loading/loading.component';
+import { IS_MOBILE } from './shared/services/is-mobile.service';
+import { NavBarComponent } from './shared/layout/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ToastModule,
+    LoadingComponent,
+    NavBarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ChronoBox';
+  private router = inject(Router);
+  public isMobile = inject(IS_MOBILE);
+  public showNavBar = signal(false);
+
+  constructor() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNavBar.set(['/home', '/profile'].includes(event.urlAfterRedirects))
+      }
+    })
+  }
 }
